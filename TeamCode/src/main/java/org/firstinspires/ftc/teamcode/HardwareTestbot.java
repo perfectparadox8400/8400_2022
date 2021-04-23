9e3a7288f57db045e18a5bb4a21ffdc236791346
bullochmarc01 <bullochmarc01@gmail.com>
2021-04-22 20:39:43 -0600

    Set up for new robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTestbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTestbot.java
@@ -53,15 +53,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class HardwareTestbot
 {
     /* Public OpMode members. */
-    public DcMotor  leftDrive   = null;
-    public DcMotor  rightDrive  = null;
-    public DcMotor  leftArm     = null;
-    public Servo    leftClaw    = null;
-    public Servo    rightClaw   = null;
-
-    public static final double MID_SERVO       =  0.5 ;
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
+    public DcMotor  lf  = null;
+    public DcMotor  lb  = null;
+    public DcMotor  rf  = null;
+    public DcMotor  rb  = null;
 
     /* local OpMode members. */
     HardwareMap hwMap           =  null;
@@ -78,28 +73,26 @@ public class HardwareTestbot
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
-        rightDrive = hwMap.get(DcMotor.class, "right_drive");
-        leftArm    = hwMap.get(DcMotor.class, "left_arm");
-        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        lf = hwMap.get(DcMotor.class, "lf");
+        rf = hwMap.get(DcMotor.class, "rf");
+        lb = hwMap.get(DcMotor.class, "lb");
+        rb = hwMap.get(DcMotor.class, "rb");
+        lb.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        rb.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to zero power
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        leftArm.setPower(0);
+        lf.setPower(0);
+        rf.setPower(0);
+        lb.setPower(0);
+        rb.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
-        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // Define and initialize ALL installed servos.
-        leftClaw  = hwMap.get(Servo.class, "left_hand");
-        rightClaw = hwMap.get(Servo.class, "right_hand");
-        leftClaw.setPosition(MID_SERVO);
-        rightClaw.setPosition(MID_SERVO);
     }
  }
 
