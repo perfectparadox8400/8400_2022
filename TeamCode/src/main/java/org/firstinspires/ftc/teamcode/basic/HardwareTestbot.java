403c722d925a7bc4477a36092dc410e9fea0628c
bullochmarc01 <bullochmarc01@gmail.com>
2021-04-26 15:39:08 -0600

    Moved Stuff to orgamize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basic/HardwareTestbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basic/HardwareTestbot.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.basic;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
